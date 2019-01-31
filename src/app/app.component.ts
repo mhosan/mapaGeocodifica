ec1068e609cb3328bae35d1c55d8ae7f79343eb1
mhosan <mhosan@gmail.com>
2019-01-31 16:16:52 -0300

    hoy es jueves 31 de enero de 2019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,9 +22,8 @@ export class AppComponent {
   //comentario
 
   geocodificar(){
-  
-    //locationiq, https://locationiq.com:
-    //Your Key is 7c041d46548c08
+  //locationiq, https://locationiq.com:
+  //Your Key is 7c041d46548c08
   //   this.json.getData("https://us1.locationiq.org/v1/search.php?key=7c041d46548c08&q=" + this.direccion + "&format=json").subscribe(respuesta => {
   //     console.log(respuesta);
   //     console.log(respuesta[0].lat + ", " + respuesta[0].lon);
@@ -32,12 +31,10 @@ export class AppComponent {
   //     this.latitudLongitud = {lat : respuesta[0].lat, lon : respuesta[0].lon};
   //     this.mapita.ponerMarcador(this.latitudLongitud);
   // });
-
-    // this.json.getData("https://us1.locationiq.org/v1/search.php?key=7c041d46548c08&q=Empire%20State%20Building&format=json").subscribe(respuesta => {
-    //   console.log(respuesta);
-    // });
-
-    this.json.getData("https://geocode.xyz/" + this.direccion + "?json=1").subscribe(respuesta => {
+  // this.json.getData("https://us1.locationiq.org/v1/search.php?key=7c041d46548c08&q=Empire%20State%20Building&format=json").subscribe(respuesta => {
+  //   console.log(respuesta);
+  // });
+  this.json.getData("https://geocode.xyz/" + this.direccion + "?json=1").subscribe(respuesta => {
       console.log(respuesta);
       console.log(respuesta.latt + ", " + respuesta.longt);
       this.latLon = respuesta.latt + ", " + respuesta.longt;
@@ -45,7 +42,6 @@ export class AppComponent {
       this.mapita.ponerMarcador(this.latitudLongitud);      
     });
   }
-
   mostrar(){
     alert("si");
   //   alert(lati + "," + longi);
@@ -58,7 +54,6 @@ export class AppComponent {
   //   //   })
   //   // }).addTo(miMapa);
   // }
-
 }
 }
 //Geocodificacion desde la api de la pagina https://geocode.xyz/api. Es gratis para un limite de velocidad de hasta 1 llamada API por segundo.
@@ -68,18 +63,3 @@ export class AppComponent {
 //avda. 7 esquina 38:       -34.906542,-57.961084
 //plaza olazabal (7 y 39):  -34.90657, -57.96112 
 //7 y 45:                   -34.912036, -57.954601
-
-
-//poblacion:
-//http://api.population.io/1.0/population/World/today-and-tomorrow/?format=json
-//http://api.population.io/1.0/population/Argentina/today-and-tomorrow/?format=json
-
-//reddit:Reddit es un sitio web de marcadores sociales y agregador de noticias donde los usuarios pueden dejar enlaces a contenidos web. 
-//Otros usuarios pueden votar a favor o en contra de los enlaces, haciendo que aparezcan más o menos destacados
-//https://www.reddit.com/r/all.json
-//https://www.reddit.com/r/AskReddit.json
-//https://www.reddit.com/r/pics.json
-//https://www.reddit.com/r/worldnews.json
-
-//peliculas:
-//https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json
